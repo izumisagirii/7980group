faf7434f074e96a0844ec9b2c044eaf7fb8f4fef
Yongheng CUI <cuiyongheng1@outlook.com>
2024-04-17 03:42:24 +0800

    update front

---
+++ api/HttpTrigger1/index.js
@@ -0,0 +1,40 @@
+const fetch = require('node-fetch');
+
+module.exports = async function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+
+    try {
+        const parsedUrl = new URL(req.url.replaceAll("/api", ""));
+        const url = new URL(`https://grouphweb.azurewebsites.net${parsedUrl.pathname}${parsedUrl.search}`);
+
+        const options = {
+            method: req.method,
+            headers: req.headers
+        };
+
+        options.headers.host = url.host;
+
+        if (req.method !== 'GET') {
+            options.body = req.bufferBody;
+        } else if (req.bufferBody) {
+            context.res = {
+                status: 400,
+                body: 'GET requests cannot have a body',
+            };
+            return;
+        }
+
+        const fetchResp = await fetch(url, options);
+        const responseMessage = await fetchResp.text();
+
+        context.res = {
+            status: fetchResp.status,
+            body: responseMessage,
+        };
+    } catch (err) {
+        context.res = {
+            status: 500,
+            body: err.message,
+        };
+    }
+};
